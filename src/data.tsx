f85c77f9d6e61a02cb4afe98b8068fe7468abaeb
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-07-13 00:26:22 +0200

    add dummy data

---
+++ src/data.tsx
@@ -0,0 +1,86 @@
+import { FaRegMap, FaUtensils, FaTableTennis, FaHome } from "react-icons/fa";
+
+type Resources = {
+  title: string;
+  link: string;
+  icon: React.ReactNode;
+  description: string;
+}[];
+
+export const resources: Resources = [
+  {
+    title: "MazeMap",
+    link:
+      "https://use.mazemap.com/#v=1&zlevel=1&left=10.7153450&right=10.7543490&top=59.9261840&bottom=59.9165320&campusid=53",
+    icon: <FaRegMap />,
+    description: "Kart som hjelper deg å finne frem til riktig rom på campus."
+  },
+  {
+    title: "Middag",
+    link: "https://www.sio.no/mat-og-drikke",
+    icon: <FaUtensils />,
+    description: "Se hva som er dagen på de ulike SiO spisestedene."
+  },
+  {
+    title: "Book bane",
+    link: "https://www.sio.no/trening/haller-og-baner/book-hall-eller-bane#/",
+    icon: <FaTableTennis />,
+    description: "Her kan du booke squash- og tennisbaner."
+  },
+  {
+    title: "SiO Bolig",
+    link: "https://www.sio.no/minside/min-okonomi/boligokonomi",
+    icon: <FaHome />,
+    description: "Se informasjon om dine leieforhold og statistikk hos SiO."
+  },
+  {
+    title: "MazeMap",
+    link:
+      "https://use.mazemap.com/#v=1&zlevel=1&left=10.7153450&right=10.7543490&top=59.9261840&bottom=59.9165320&campusid=53",
+    icon: <FaRegMap />,
+    description: "Kart som hjelper deg å finne frem til riktig rom på campus."
+  },
+  {
+    title: "Middag",
+    link: "https://www.sio.no/mat-og-drikke",
+    icon: <FaUtensils />,
+    description: "Se hva som er dagen på de ulike SiO spisestedene."
+  },
+  {
+    title: "Book bane",
+    link: "https://www.sio.no/trening/haller-og-baner/book-hall-eller-bane#/",
+    icon: <FaTableTennis />,
+    description: "Her kan du booke squash- og tennisbaner."
+  },
+  {
+    title: "SiO Bolig",
+    link: "https://www.sio.no/minside/min-okonomi/boligokonomi",
+    icon: <FaHome />,
+    description: "Se informasjon om dine leieforhold og statistikk hos SiO."
+  },
+  {
+    title: "MazeMap",
+    link:
+      "https://use.mazemap.com/#v=1&zlevel=1&left=10.7153450&right=10.7543490&top=59.9261840&bottom=59.9165320&campusid=53",
+    icon: <FaRegMap />,
+    description: "Kart som hjelper deg å finne frem til riktig rom på campus."
+  },
+  {
+    title: "Middag",
+    link: "https://www.sio.no/mat-og-drikke",
+    icon: <FaUtensils />,
+    description: "Se hva som er dagen på de ulike SiO spisestedene."
+  },
+  {
+    title: "Book bane",
+    link: "https://www.sio.no/trening/haller-og-baner/book-hall-eller-bane#/",
+    icon: <FaTableTennis />,
+    description: "Her kan du booke squash- og tennisbaner."
+  },
+  {
+    title: "SiO Bolig",
+    link: "https://www.sio.no/minside/min-okonomi/boligokonomi",
+    icon: <FaHome />,
+    description: "Se informasjon om dine leieforhold og statistikk hos SiO."
+  }
+];

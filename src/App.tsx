a4660b428aee57a2e6eaf21a2dfe0b801fefe281
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-07-13 15:34:46 +0200

    Write html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import React from "react";
+import { FaHome } from "react-icons/fa";
+import "./App.scss";
 
 const App: React.FC = () => {
   return (
-    <div>
-      Hello World
+    <div className="page-container">
+      <header className="header">Header</header>
+      <main className="main">
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+        <div className="resource-card">
+          <div className="card-content">
+            <h1 className="card-title">MazeMap</h1>
+            <span className="card-icon">
+              <FaHome />
+            </span>
+          </div>
+        </div>
+      </main>
+      <footer className="footer">Footer</footer>
     </div>
   );
-}
+};
 
 export default App;

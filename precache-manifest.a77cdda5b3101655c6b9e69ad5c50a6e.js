e3ef5696da6e3c43e6b557b8dc3c09edb3a65eaa
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-07-14 00:30:34 +0200

    Updates

---
+++ precache-manifest.a77cdda5b3101655c6b9e69ad5c50a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c057d93d170da63f7a09667a7ee2d2",
+    "url": "/met-i-blinken/index.html"
+  },
+  {
+    "revision": "f6a3b5e004160bdb1b8c",
+    "url": "/met-i-blinken/static/css/main.c5e94fbc.chunk.css"
+  },
+  {
+    "revision": "c0bf2771583bf7233ff0",
+    "url": "/met-i-blinken/static/js/2.2ad78bb9.chunk.js"
+  },
+  {
+    "revision": "f6a3b5e004160bdb1b8c",
+    "url": "/met-i-blinken/static/js/main.f137eb9c.chunk.js"
+  },
+  {
+    "revision": "0a080c223cf4d262e5ca",
+    "url": "/met-i-blinken/static/js/runtime~main.07acc18f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/met_i_blinken/precache-manifest.c2166aac875c904d5e7a005fe6925d31.js"
+  "/met-i-blinken/precache-manifest.a77cdda5b3101655c6b9e69ad5c50a6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/met_i_blinken/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/met-i-blinken/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

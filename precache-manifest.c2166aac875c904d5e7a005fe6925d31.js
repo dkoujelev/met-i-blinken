5ace70ed99203f135beaa0acaa1e6b0d7897fcd6
Dmitry Koujelev <dkoujelev97@gmail.com>
2019-07-14 00:28:27 +0200

    Updates

---
+++ precache-manifest.c2166aac875c904d5e7a005fe6925d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4679e19081c020af70c2c26a68028b",
+    "url": "/met_i_blinken/index.html"
+  },
+  {
+    "revision": "b72ba5ed374a7bc2f4e8",
+    "url": "/met_i_blinken/static/css/main.c5e94fbc.chunk.css"
+  },
+  {
+    "revision": "c0bf2771583bf7233ff0",
+    "url": "/met_i_blinken/static/js/2.2ad78bb9.chunk.js"
+  },
+  {
+    "revision": "b72ba5ed374a7bc2f4e8",
+    "url": "/met_i_blinken/static/js/main.37b8abe8.chunk.js"
+  },
+  {
+    "revision": "369981889716ea121f36",
+    "url": "/met_i_blinken/static/js/runtime~main.39a52618.js"
+  }
+]);

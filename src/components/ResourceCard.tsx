4a66b648ced0bcd13cf4f6916a332af11b4889be
Dmitryko97 <dkoujelev97@gmail.com>
2019-08-07 16:55:49 +0200

    Open links in new tab

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -3,7 +3,12 @@ import { Resource } from "../data";
 
 export const ResourceCard = ({ title, link, icon }: Resource) => {
   return (
-    <a href={link} className="resource-card">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="resource-card"
+    >
       <div className="card-content">
         <h1 className="card-title">{title}</h1>
         <span className="card-icon">{icon}</span>

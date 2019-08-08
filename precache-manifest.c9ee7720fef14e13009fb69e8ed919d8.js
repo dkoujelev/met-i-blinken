22d10c3079a6d4e11be3fff1c215c19d7ce3f7b6
Dmitryko97 <dkoujelev97@gmail.com>
2019-08-08 17:39:37 +0200

    Updates

---
+++ precache-manifest.c9ee7720fef14e13009fb69e8ed919d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502d36f3da2d60119475e05d364a8f16",
+    "url": "/met-i-blinken/index.html"
+  },
+  {
+    "revision": "5d978574c1d6ce7c806d",
+    "url": "/met-i-blinken/static/css/main.9055cd9f.chunk.css"
+  },
+  {
+    "revision": "ee7f07b5f468028d5d38",
+    "url": "/met-i-blinken/static/js/2.7e7ad7c3.chunk.js"
+  },
+  {
+    "revision": "5d978574c1d6ce7c806d",
+    "url": "/met-i-blinken/static/js/main.dbe32779.chunk.js"
+  },
+  {
+    "revision": "0a080c223cf4d262e5ca",
+    "url": "/met-i-blinken/static/js/runtime~main.07acc18f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/met-i-blinken/precache-manifest.92e0cd5a19d16cedfe9bdf64a6bc0509.js"
+  "/met-i-blinken/precache-manifest.c9ee7720fef14e13009fb69e8ed919d8.js"
 );
 
 self.addEventListener('message', (event) => {
